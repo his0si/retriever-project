c83469152ad2b1b2ae5973a0455c8e159144360e
his0si <his0si2276@gmail.com>
2025-11-17 16:28:07 +0900

    docker prod

--- backend/celery_app.py
+++ backend/celery_app.py
@@ -21,6 +21,7 @@ celery_app.conf.update(
     task_soft_time_limit=4 * 60 * 60,  # 4 hours (240 minutes)
     worker_prefetch_multiplier=1,
     worker_max_tasks_per_child=1000,
+    broker_connection_retry_on_startup=True,  # Celery 6.0 호환성
     # Queue routing: separate embedding tasks to dedicated queue
     task_routes={
         'process_url_for_embedding': {'queue': 'embedding'},
